8ade4074b00e27e5f8754528363450b9e5ff3b3b
NandaMarsol <fernanda.almferreira@gmail.com>
2020-10-27 10:53:26 -0300

    Primeiro commit - Let's Go!

---
+++ api-cartaobranco-proposta/src/main/java/br/com/zup/apicartaobrancoproposta/ApiCartaobrancoPropostaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.apicartaobrancoproposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCartaobrancoPropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCartaobrancoPropostaApplication.class, args);
+	}
+
+}
